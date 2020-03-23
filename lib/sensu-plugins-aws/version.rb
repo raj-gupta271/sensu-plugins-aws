6a7b7a7dda5d4392106d237d19d883516b6ffe7d
Nicolas Boutet <amd3002@gmail.com>
2020-03-23 14:37:16 +0100

    Update name

--- lib/sensu-plugins-aws/version.rb
+++ lib/sensu-plugins-aws/version.rb
@@ -1,8 +1,7 @@
 module SensuPluginsAWS
   module Version
-    MAJOR = 18
-    MINOR = 5
-    PATCH = 0
-    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
+    UPSTREAM = 18.5.0
+    FORK_REV = 1
+    VER_STRING = [UPSTREAM, FORK_REV].compact.join('-')
   end
 end
